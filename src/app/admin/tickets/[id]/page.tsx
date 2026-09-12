b8b2cd70fbfe9edd8abfd49e7d4712b8a8f7ea64
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-09-12 22:47:25 +0100

    Admin ticket detail page now has : AI analysis panel, AI Draft reply displayed and re-analyze button added. AI Draft still needs linking to SendReply for tracking & Comparing AI Draft to Final send to evaluate edits

--- src/app/admin/tickets/[id]/page.tsx
+++ src/app/admin/tickets/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
-import { sendReply } from "@/actions/tickets";
+import { reanalyzeTicket, sendReply } from "@/actions/tickets";
 
 export default async function AdminTicketDetailPage({ params } : { params : Promise <{id : string }> }){
 	const { id } = await params;
@@ -14,21 +14,53 @@ export default async function AdminTicketDetailPage({ params } : { params : Prom
 
 	if (!ticket) return null;
 
+	const draft = ticket.messages.find(
+		(message) => message.type === "AI_DRAFT" && message.draftStatus === "PENDING");
 
 	return (
 		<div>
 			<h1>{ticket.subject}</h1>
 			<p>Status: {ticket.status}</p>
 			<div>
-				{ticket.messages.map((message) => (
+				{ticket.messages
+				.filter((message) => message.type !== "AI_DRAFT")
+				.map((message) => (
 					<p key={message.id}>{message.content}</p>
 				))}
+			</div>
+
+			<div>
+				<h2>AI Analysis</h2>
+				{ticket.aiAnalysisError ? 
+					(<p>Error: {ticket.aiAnalysisError}</p>)
+					:
+					( <div>
+					<p>Category: {ticket.category}</p>
+					<p>Priority: {ticket.priority}</p>
+					<p>Sentiment: {ticket.sentiment}</p>
+					  </div>
+					)
+				}
+			</div>
+
+			<div>
+				<h3>AI Draft</h3>
+				{draft ?
+				(<p>AI Draft: {draft.content}</p>)
+				:
+				(<p>AI Draft unavailable</p>)
+				}
 			</div>
 
 			 <form action={sendReply}>
 			<input type="hidden" name="ticketId" value={ticket.id}></input>
 			<textarea name="content" placeholder="Write a reply"></textarea>
 			<button type="submit">Send Reply</button>
+			 </form>
+
+			 <form action={reanalyzeTicket}>
+				<input type="hidden" name="ticketId" value={ticket.id}></input>
+				<button type="submit">Reanalyze ticket</button>
 			 </form>
 		</div>
 	)
